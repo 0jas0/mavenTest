35ea667bb08ae76cbd5d5c95429ca90675a9d8e9
琚安生 <juansheng@qijiayoudao.com>
2019-07-22 20:44:52 +0800

    增加es

--- src/main/java/com/jas/elasticSearch/ElasticSearchDAOImpl.java
+++ src/main/java/com/jas/elasticSearch/ElasticSearchDAOImpl.java
@@ -1,14 +1,24 @@
 package com.jas.elasticSearch;
 
+import com.alibaba.fastjson.JSON;
+import io.searchbox.action.Action;
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestResult;
-import io.searchbox.indices.ClearCache;
-import io.searchbox.indices.DeleteIndex;
-import io.searchbox.action.Action;
+import io.searchbox.cluster.Health;
+import io.searchbox.cluster.NodesInfo;
+import io.searchbox.cluster.NodesStats;
+import io.searchbox.core.*;
+import io.searchbox.indices.*;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <pre>
@@ -33,6 +43,119 @@ public class ElasticSearchDAOImpl implements IElasticSearchDAO{
     public JestResult clearCache() {
         ClearCache clearCache = new ClearCache.Builder().build();
         return execute(clearCache, "清空缓存失败");
+    }
+
+    @Override
+    public JestResult closeIndex(String indexName) {
+        CloseIndex closeIndex = new CloseIndex.Builder(indexName).build();
+        return execute(closeIndex, "关闭索引失败，indexName:" + indexName);
+    }
+
+    @Override
+    public JestResult optimizeIndex() {
+        Optimize optimize = new Optimize.Builder().build();
+        return execute(optimize, "优化索引失败");
+    }
+
+    @Override
+    public JestResult flushIndex() {
+        Flush flush = new Flush.Builder().build();
+        return execute(flush, "优化索引失败");
+    }
+
+    @Override
+    public JestResult indicsExists(String indexName) {
+        IndicesExists indicesExists = new IndicesExists.Builder(indexName).build();
+        return execute(indicesExists, "判断索引是否存在失败，indexName:" + indexName);
+    }
+
+    @Override
+    public JestResult nodesInfo() {
+        NodesInfo info = new NodesInfo.Builder().build();
+        return execute(info, "获取节点信息失败");
+    }
+
+    @Override
+    public JestResult health() {
+        Health health = new Health.Builder().build();
+        return execute(health, "获取健康信息失败");
+    }
+
+    @Override
+    public JestResult nodesStats() {
+        NodesStats nodesStats = new NodesStats.Builder().build();
+        return execute(nodesStats, "获取节点的状态信息");
+    }
+
+    @Override
+    public <T> JestResult updateDocument(T o, String index, String type, String id) {
+        Map<String, T> map = new HashMap<>();
+        map.put("doc", o);
+        String string = JSON.toJSONString(o);
+        Update update = new Update.Builder(string).index(index).type(type).id(id).build();
+        return execute(update,"更新文档信息失败");
+    }
+
+    @Override
+    public JestResult deleteDocument(String index, String type, String id) {
+        Delete delete = new Delete.Builder(id).index(index).type(type).build();
+        StringBuilder stringBuilder = new StringBuilder("通过主键方式删除文档失败");
+        stringBuilder.append(",index:" + index);
+        stringBuilder.append(",type:" + type);
+        stringBuilder.append(",id:" + id);
+        return execute(delete, stringBuilder.toString());
+    }
+
+    @Override
+    public JestResult deleteDocumentByQuery(String index, String type, String params) {
+        DeleteByQuery deleteByQuery = new DeleteByQuery.Builder(params).addIndex(index).addType(type).build();
+        StringBuilder stringBuilder = new StringBuilder("通过添加删除文档失败");
+        stringBuilder.append(",index:" + index);
+        stringBuilder.append(",type:" + type);
+        stringBuilder.append(",params:" + params);
+        return execute(deleteByQuery, stringBuilder.toString());
+    }
+
+    @Override
+    public JestResult getDocument(String index, String type, String id) {
+        Get get = new Get.Builder(index,id).type(type).build();
+        StringBuilder stringBuilder = new StringBuilder("通过id获取文档失败");
+        stringBuilder.append(",index:" + index);
+        stringBuilder.append(",type:" + type);
+        stringBuilder.append(",id:" + id);
+        return execute(get, stringBuilder.toString());
+    }
+
+    @Override
+    public JestResult createSearch(String keyword, String index, String type, String... fields) {
+        if (fields == null || fields.length == 0){
+            return null;
+        }
+        BoolQueryBuilder booleanQueryBuilder = QueryBuilders.boolQuery();
+        for (String field : fields){
+            BoolQueryBuilder innerQuery = QueryBuilders.boolQuery();
+            innerQuery.filter(QueryBuilders.termQuery(field, keyword));
+            booleanQueryBuilder.should(innerQuery);
+        }
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(booleanQueryBuilder);
+        Search search = new Search.Builder(searchSourceBuilder.toString()).build();
+        StringBuilder stringBuilder = new StringBuilder("模糊查询失败");
+        stringBuilder.append(",keyword:" + keyword);
+        stringBuilder.append(",index:" + index);
+        stringBuilder.append(",type:" + type);
+        stringBuilder.append(",fields:" + Arrays.toString(fields));
+        return execute(search, stringBuilder.toString());
+    }
+
+    @Override
+    public <T> JestResult createIndex(T o, String indexName, String type) {
+        Index index = new Index.Builder(o).index(indexName).type(type).build();
+        StringBuilder stringBuilder = new StringBuilder("创建索引失败");
+        stringBuilder.append(",index:" + indexName);
+        stringBuilder.append(",type:" + type);
+        stringBuilder.append(",object:" + JSON.toJSONString(o));
+        return execute(index, stringBuilder.toString());
     }
 
     private JestResult execute(Action clientRequest, String msg){
--- src/main/java/com/jas/elasticSearch/IElasticSearchDAO.java
+++ src/main/java/com/jas/elasticSearch/IElasticSearchDAO.java
@@ -27,4 +27,142 @@ public interface IElasticSearchDAO {
       * @return: io.searchbox.client.JestResult
     */
     public JestResult clearCache();
+
+
+    /**
+      * @Description: 关闭索引
+      * @Author: juas
+      * @Date: 2019/7/22 7:10 PM
+      * @return: io.searchbox.client.JestResult
+    */
+    public JestResult closeIndex(String indexName);
+
+
+    /**
+      * @Description: 优化索引
+      * @Author: juas
+      * @Date: 2019/7/22 7:11 PM
+      * @return: io.searchbox.client.JestResult
+    */
+    public JestResult optimizeIndex();
+
+
+    /**
+      * @Description: 刷新索引
+      * @Author: juas
+      * @Date: 2019/7/22 7:12 PM
+      * @return: io.searchbox.client.JestResult
+    */
+    public JestResult flushIndex();
+
+
+    /**
+      * @Description: 判断索引是否存在
+      * @Author: juas
+      * @Date: 2019/7/22 7:13 PM
+     *  @parm indexName
+      * @return: io.searchbox.client.JestResult
+    */
+    public JestResult indicsExists(String indexName);
+
+
+    /***
+      * @Description: 查看节点信息
+      * @Author: juas
+      * @Date: 2019/7/22 7:14 PM
+      * @return: io.searchbox.client.JestResult
+    */
+    public JestResult nodesInfo();
+
+
+    /**
+      * @Description: 查看集群健康情况
+      * @Author: juas
+      * @Date: 2019/7/22 7:14 PM
+      * @return: io.searchbox.client.JestResult
+    */
+    public JestResult health();
+
+
+    /**
+      * @Description: 查看节点状态
+      * @Author: juas
+      * @Date: 2019/7/22 7:15 PM
+      * @return: io.searchbox.client.JestResult
+    */
+    public JestResult nodesStats();
+
+
+    /**
+      * @Description: 更新document
+      * @Author: juas
+      * @Date: 2019/7/22 7:16 PM
+      * @Param doc: 更新后的信息
+      * @Param index: 索引库
+      * @Param type: 对象类型
+      * @Param id: 文档唯一表示
+      * @return: io.searchbox.client.JestResult
+    */
+    public <T> JestResult updateDocument(T o, String index, String type, String id);
+
+
+    /**
+      * @Description: 删除一个文档通过id
+      * @Author: juas
+      * @Date: 2019/7/22 7:18 PM
+      * @Param index:
+      * @Param type:
+      * @Param id:
+      * @return: io.searchbox.client.JestResult
+    */
+    public JestResult deleteDocument(String index, String type, String id);
+
+
+    /**
+      * @Description: 通过条件删除文档
+      * @Author: juas
+      * @Date: 2019/7/22 7:20 PM
+      * @Param index:
+      * @Param type:
+      * @Param params:
+      * @return: io.searchbox.client.JestResult
+    */
+    public JestResult deleteDocumentByQuery(String index, String type, String params);
+
+
+    /**
+      * @Description: 获取一个文档
+      * @Author: juas
+      * @Date: 2019/7/22 7:22 PM
+      * @Param index:
+      * @Param type:
+      * @Param id:
+      * @return: io.searchbox.client.JestResult
+    */
+    public JestResult getDocument(String index, String type, String id);
+
+
+    /**
+      * @Description: 搜索关键词
+      * @Author: juas
+      * @Date: 2019/7/22 7:33 PM
+      * @Param keyword: 搜索的关键词
+      * @Param index:
+      * @Param type:
+      * @Param fields: 要搜索的字段
+      * @return: io.searchbox.client.JestResult
+    */
+    public JestResult createSearch(String keyword, String index, String type, String... fields);
+
+
+    /**
+      * @Description: 创建一个索引
+      * @Author: juas
+      * @Date: 2019/7/22 7:39 PM
+      * @Param o:
+      * @Param index:
+      * @Param type:
+      * @return: io.searchbox.client.JestResult
+    */
+    public <T> JestResult createIndex(T o , String index , String type);
 }
